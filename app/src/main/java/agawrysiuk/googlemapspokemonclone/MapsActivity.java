90c28f1188c83237bca1491a0ec07ddaec306ede
dlrzz <arkad.gawry@gmail.com>
2020-01-15 10:47:41 +0100

    Implemented locating behavior, added player's icon

--- app/src/main/java/agawrysiuk/googlemapspokemonclone/MapsActivity.java
+++ app/src/main/java/agawrysiuk/googlemapspokemonclone/MapsActivity.java
@@ -1,19 +1,38 @@
 package agawrysiuk.googlemapspokemonclone;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.parse.ParseUser;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    private static final int LOCATION_REQUEST_CODE = 1000;
+    private static final int RESIZE_MULTIPLIER = 7;
+
     private GoogleMap mMap;
+    private LocationManager mLocationManager;
+    private LocationListener mLocationListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,14 +54,78 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+    @SuppressLint("MissingPermission")
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 20));
+        // == setting up manager and listener ==
+        mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        mLocationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                // == we update player's location with location change
+                updateYourLocation(location);
+            }
 
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+            }
+        };
+
+        // == request gps access ==
+        if (Build.VERSION.SDK_INT < 23) {
+            mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, mLocationListener);
+        } else if (Build.VERSION.SDK_INT >= 23) {
+            if (ContextCompat.checkSelfPermission(MapsActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(MapsActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_REQUEST_CODE);
+            } else {
+                mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, mLocationListener);
+                Location location = mLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                updateYourLocation(location);
+            }
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        // == after request gps ==
+        if (requestCode == 1000 && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            if (ContextCompat.checkSelfPermission(MapsActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, mLocationListener);
+                Location location = mLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                updateYourLocation(location);
+            }
+        }
+    }
+
+    // == update players location ==
+    private void updateYourLocation(Location location) {
+        LatLng yourLocation = new LatLng(location.getLatitude(), location.getLongitude());
+        mMap.clear();
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(yourLocation, 20));
+        mMap.addMarker(
+                new MarkerOptions()
+                        .position(yourLocation)
+                        .title(ParseUser.getCurrentUser().getUsername())
+                        .icon(BitmapDescriptorFactory.fromBitmap(resizeMapIcons(R.drawable.player_back))));
+
+
+    }
+
+    public Bitmap resizeMapIcons(int iconDrawable){
+        Bitmap imageBitmap = BitmapFactory.decodeResource(MapsActivity.this.getResources(),iconDrawable);
+//        Bitmap imageBitmap = BitmapFactory.decodeResource(getResources(),getResources().getIdentifier(iconName, "drawable", getPackageName()));
+        return Bitmap.createScaledBitmap(imageBitmap, imageBitmap.getWidth() * RESIZE_MULTIPLIER, imageBitmap.getHeight() * RESIZE_MULTIPLIER, false);
     }
 }
